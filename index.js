0baef1d609ab6964a20dac7641d55c247ada1797
nittish03 <nittishbaboria03@gmail.com>
2024-01-19 00:00:30 +0530

    added the new button

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 let a=document.getElementById("battery");
 let b=document.getElementById("start");
-if(b.textContent=="START"){
-    b.onclick=()=>{
-b.textContent="STOP";
-a.style.animation="animation 6s 0s ease-out infinite normal";
-b.remove();
+let cb=document.createElement("button");
+cb.textContent=("STOP");
+cb.setAttribute("id","stop")
+let c=document.getElementById("container");
+cb.onclick=()=>{
+    a.style.animation="";
+    document.location.reload();
+    
+}
+b.onclick=()=>{
+    b.textContent="STOP";
+    a.style.animation="animation 6s 0s ease-out infinite normal";
+    b.remove();
+    c.after(cb);
+    }
 
-}}  
